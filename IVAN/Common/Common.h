51e4fdc5029002fe32589979c477a6fa62f5a9bd
maoyingxue <ymao46972@gmail.com>
2019-09-05 00:47:18 +0800

    Update Common.h

--- IVAN/Common/Common.h
+++ IVAN/Common/Common.h
@@ -1,11 +1,3 @@
-/******************************************
-   file name: Common.h
-   create time: 2017年12月28日 星期四 13时53分46秒
-   author: Jipeng Huang
-   e-mail: huangjipengnju@gmail.com
-   github: https://github.com/hjptriplebee
-   details: Header set of common
-******************************************/
 #ifndef COMMON_H
 #define COMMON_H
 //outer library
